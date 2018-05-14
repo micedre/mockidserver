caf7804c8a1ce028ccff93403653e8fcd2850970
Cédric Couralet <cedric.couralet@insee.fr>
2018-05-14 07:48:29 +0200

    Init project (Spring Boot 2)

---
+++ src/main/java/net/micedre/mockidserver/mockidserver/MockidserverApplication.java
@@ -0,0 +1,12 @@
+package net.micedre.mockidserver.mockidserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockidserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockidserverApplication.class, args);
+	}
+}
---
+++ src/test/java/net/micedre/mockidserver/mockidserver/MockidserverApplicationTests.java
@@ -0,0 +1,16 @@
+package net.micedre.mockidserver.mockidserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockidserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
